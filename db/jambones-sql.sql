4e81172c73dfbecd8edf8609ad5a6cb6e15ad6b3
Dave Horton <daveh@beachdognet.com>
2019-12-10 10:36:28 -0500

    add support for basic auth when calling registration_hook

--- db/jambones-sql.sql
+++ db/jambones-sql.sql
@@ -134,6 +134,8 @@ CREATE TABLE IF NOT EXISTS `service_providers`
 `description` VARCHAR(255),
 `root_domain` VARCHAR(255) UNIQUE ,
 `registration_hook` VARCHAR(255),
+`hook_basic_auth_user` VARCHAR(255),
+`hook_basic_auth_password` VARCHAR(255),
 PRIMARY KEY (`service_provider_sid`)
 ) ENGINE=InnoDB COMMENT='An organization that provides communication services to its ';
 
@@ -162,6 +164,8 @@ CREATE TABLE IF NOT EXISTS `accounts`
 `sip_realm` VARCHAR(255) UNIQUE ,
 `service_provider_sid` CHAR(36) NOT NULL,
 `registration_hook` VARCHAR(255),
+`hook_basic_auth_user` VARCHAR(255),
+`hook_basic_auth_password` VARCHAR(255),
 PRIMARY KEY (`account_sid`)
 ) ENGINE=InnoDB COMMENT='A single end-user of the platform';
 
