5d5bd223cd78e715c5ce2f5de51904f079dda733
Dave Horton <daveh@beachdognet.com>
2020-04-21 09:27:54 -0400

    add script to create default service provider and account to database

---
+++ db/create-default-account.sql
@@ -0,0 +1,4 @@
+insert into service_providers (service_provider_sid, name) 
+values ('2708b1b3-2736-40ea-b502-c53d8396247f', 'default service provider');
+insert into accounts (account_sid, service_provider_sid, name) 
+values ('9351f46a-678c-43f5-b8a6-d4eb58d131af','2708b1b3-2736-40ea-b502-c53d8396247f', 'default account');
