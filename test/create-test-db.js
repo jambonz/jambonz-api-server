ab7c69c0e8110a22a13c5356bb7435e77c20dd28
Dave Horton <daveh@beachdognet.com>
2021-05-07 08:38:54 -0400

    fix test case

--- test/create-test-db.js
+++ test/create-test-db.js
@@ -2,7 +2,7 @@ const test = require('blue-tape').test ;
 const exec = require('child_process').exec ;
 
 test('creating jambones_test database', (t) => {
-  exec(`mysql -h 127.0.0.1 -u root --protocol=tcp < ${__dirname}/../db/create_test_db.sql`, (err, stdout, stderr) => {
+  exec(`mysql -h 127.0.0.1 -u root --protocol=tcp --port=3360 < ${__dirname}/../db/create_test_db.sql`, (err, stdout, stderr) => {
     if (err) return t.end(err);
     t.pass('database successfully created');
     t.end();
@@ -10,7 +10,7 @@ test('creating jambones_test database', (t) => {
 });
 
 test('creating schema', (t) => {
-  exec(`mysql -h 127.0.0.1 -u root --protocol=tcp -D jambones_test < ${__dirname}/../db/jambones-sql.sql`, (err, stdout, stderr) => {
+  exec(`mysql -h 127.0.0.1 -u root --protocol=tcp  --port=3360 -D jambones_test < ${__dirname}/../db/jambones-sql.sql`, (err, stdout, stderr) => {
     if (err) return t.end(err);
     t.pass('schema successfully created');
     t.end();
@@ -18,7 +18,7 @@ test('creating schema', (t) => {
 });
 
 test('creating auth token', (t) => {
-  exec(`mysql -h 127.0.0.1 -u root  --protocol=tcp -D jambones_test < ${__dirname}/../db/create-admin-token.sql`, (err, stdout, stderr) => {
+  exec(`mysql -h 127.0.0.1 -u root  --protocol=tcp  --port=3360 -D jambones_test < ${__dirname}/../db/create-admin-token.sql`, (err, stdout, stderr) => {
     if (err) return t.end(err);
     t.pass('auth token successfully created');
     t.end();
