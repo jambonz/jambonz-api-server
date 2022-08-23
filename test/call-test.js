abd2652a3838733177f30a120be96546288938b8
Quan Luu <quan.luuhoang8@gmail.com>
2022-08-23 08:59:11 +0700

    wip: add integration test

---
+++ test/call-test.js
@@ -0,0 +1,174 @@
+const test = require('tape') ;
+const jwt = require('jsonwebtoken');
+const request = require('request-promise-native').defaults({
+    baseUrl: 'http://127.0.0.1:3000/v1'
+  });
+const consoleLogger = {debug: console.log, info: console.log, error: console.error}
+const {
+  createServiceProvider,
+  createAccount,
+  createGoogleSpeechCredentials,
+  getLastRequestFromFeatureServer
+} = require('./utils');
+
+test('Create Call Success Without Synthesizer in Payload', async(t) => {
+  // GIVEN
+    const app = require('../app');
+    let result;
+    const service_provider_sid = await createServiceProvider(request, 'account_has_synthesizer');
+    const account_sid = await createAccount(request, service_provider_sid, 'account_has_synthesizer');
+    const token = jwt.sign({
+        account_sid
+      }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const authUser = {bearer: token};
+    const speech_sid = await createGoogleSpeechCredentials(request, account_sid, null, authUser, true, true)
+
+    // WHEN
+    result = await request.post(`/Accounts/${account_sid}/Calls`, {
+        resolveWithFullResponse: true,
+        auth: authUser,
+        json: true,
+        body: {
+            call_hook: "https://public-apps.jambonz.us/hello-world",
+            call_status_hook:"https://public-apps.jambonz.us/call-status",
+            from: "15083778299",
+            to: {
+                type: "phone",
+                number: "15089084809"
+            }
+        }
+      });
+      // THEN
+      t.ok(result.statusCode === 201, 'successfully created Call without Synthesizer && application_sid');
+      const fs_request = await getLastRequestFromFeatureServer('15083778299_createCall');
+      const obj = JSON.parse(fs_request);
+      t.ok(obj.body.speech_synthesis_vendor == 'google', 'speech synthesizer successfully added')
+      t.ok(obj.body.speech_recognizer_vendor == 'google', 'speech recognizer successfully added')
+});
+
+test('Create Call Success Without Synthesizer in Payload and service synthesizer', async(t) => {
+  // GIVEN
+    const app = require('../app');
+    let result;
+    const service_provider_sid = await createServiceProvider(request, 'service_has_synthesizer');
+    const account_sid = await createAccount(request, service_provider_sid, 'service_has_synthesizer');
+    const token = jwt.sign({
+        account_sid
+      }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const authUser = {bearer: token};
+    const speech_sid = await createGoogleSpeechCredentials(request, null, service_provider_sid, null, true, true)
+
+    // WHEN
+    result = await request.post(`/Accounts/${account_sid}/Calls`, {
+        resolveWithFullResponse: true,
+        auth: authUser,
+        json: true,
+        body: {
+            call_hook: "https://public-apps.jambonz.us/hello-world",
+            call_status_hook:"https://public-apps.jambonz.us/call-status",
+            from: "service_has_synthesizer",
+            to: {
+                type: "phone",
+                number: "15089084809"
+            }
+        }
+      });
+      // THEN
+      t.ok(result.statusCode === 201, 'successfully created Call without Synthesizer && application_sid');
+      const fs_request = await getLastRequestFromFeatureServer('service_has_synthesizer_createCall');
+      const obj = JSON.parse(fs_request);
+      t.ok(obj.body.speech_synthesis_vendor == 'google', 'speech synthesizer successfully added')
+      t.ok(obj.body.speech_recognizer_vendor == 'google', 'speech recognizer successfully added')
+});
+
+test('Create Call Error Without Synthesizer in Payload and no setting synthesizer', async(t) => {
+  // GIVEN
+    const app = require('../app');
+    let result;
+    const service_provider_sid = await createServiceProvider(request, 'account_no_synthesizer');
+    const account_sid = await createAccount(request, service_provider_sid, 'account_no_synthesizer');
+    const token = jwt.sign({
+        account_sid
+      }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const authUser = {bearer: token};
+
+    // WHEN
+    result = await request.post(`/Accounts/${account_sid}/Calls`, {
+        resolveWithFullResponse: true,
+        auth: authUser,
+        json: true,
+        body: {
+            call_hook: "https://public-apps.jambonz.us/hello-world",
+            call_status_hook:"https://public-apps.jambonz.us/call-status",
+            from: "account_no_synthesizer",
+            to: {
+                type: "phone",
+                number: "15089084809"
+            }
+        }
+      })
+      .then(data => {t.ok(false, 'Create Call should not be success')})
+      .catch(error => {t.ok(error.response.statusCode === 400, 'Call failed for no synthesizer')});
+});
+
+test('Create Call Error Without Synthesizer in Payload and no setting synthesizer, only recognizer', async(t) => {
+  // GIVEN
+    const app = require('../app');
+    let result;
+    const service_provider_sid = await createServiceProvider(request, 'account_recognizer_no_synthesizer');
+    const account_sid = await createAccount(request, service_provider_sid, 'account_recognizer_no_synthesizer');
+    const token = jwt.sign({
+        account_sid
+      }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const authUser = {bearer: token};
+    const speech_sid = await createGoogleSpeechCredentials(request, account_sid, null, authUser, false, true)
+
+    // WHEN
+    result = await request.post(`/Accounts/${account_sid}/Calls`, {
+        resolveWithFullResponse: true,
+        auth: authUser,
+        json: true,
+        body: {
+            call_hook: "https://public-apps.jambonz.us/hello-world",
+            call_status_hook:"https://public-apps.jambonz.us/call-status",
+            from: "account_recognizer_no_synthesizer",
+            to: {
+                type: "phone",
+                number: "15089084809"
+            }
+        }
+      })
+      .then(data => {t.ok(false, 'Create Call should not be success')})
+      .catch(error => {t.ok(error.response.statusCode === 400, 'Call failed for no synthesizer')});
+});
+
+test('Create Call Error Without Synthesizer in Payload and only synthesizer, no recognizer', async(t) => {
+  // GIVEN
+    const app = require('../app');
+    let result;
+    const service_provider_sid = await createServiceProvider(request, 'account_synthesizer_no_recognizer');
+    const account_sid = await createAccount(request, service_provider_sid, 'account_synthesizer_no_recognizer');
+    const token = jwt.sign({
+        account_sid
+      }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const authUser = {bearer: token};
+    const speech_sid = await createGoogleSpeechCredentials(request, account_sid, null, authUser, true, false)
+
+    // WHEN
+    result = await request.post(`/Accounts/${account_sid}/Calls`, {
+        resolveWithFullResponse: true,
+        auth: authUser,
+        json: true,
+        body: {
+            call_hook: "https://public-apps.jambonz.us/hello-world",
+            call_status_hook:"https://public-apps.jambonz.us/call-status",
+            from: "account_synthesizer_no_recognizer",
+            to: {
+                type: "phone",
+                number: "15089084809"
+            }
+        }
+      })
+      .then(data => {t.ok(false, 'Create Call should not be success')})
+      .catch(error => {t.ok(error.response.statusCode === 400, 'Call failed for no recognizer')});
+});
---
+++ test/feature-server-test-scaffold/Dockerfile
@@ -0,0 +1,23 @@
+FROM --platform=linux/amd64 node:18.6.0-alpine as base
+
+RUN apk --update --no-cache add --virtual .builds-deps build-base python3
+
+WORKDIR /opt/app/
+
+FROM base as build
+
+COPY package.json package-lock.json ./
+
+RUN npm ci
+
+COPY . .
+
+FROM base
+
+COPY --from=build /opt/app /opt/app/
+
+ARG NODE_ENV
+
+ENV NODE_ENV $NODE_ENV
+
+CMD [ "node", "app.js" ]
