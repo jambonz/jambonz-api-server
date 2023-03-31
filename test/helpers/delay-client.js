2d6e6518eaa17f32cf4dcfc7f58374fd3f4b55dc
Markus Frindt <m.frindt@cognigy.com>
2023-03-31 21:06:33 +0200

    clean test

--- test/helpers/delay-client.js
+++ test/helpers/delay-client.js
@@ -1,29 +1,16 @@
 const test = require('tape');
-const{delayClient} = require('../../lib/helpers');
+const { delayClient } = require('../../lib/helpers');
 const bent = require('bent');
 const getJSON = bent('json')
 const logger = {
-  debug: () =>{},
-  info: () => {}
+  debug: () => { },
+  info: () => { }
 }
 
-test('delay-client-test', async(t) => {
-  // Prepare env:
-  process.env.CUSTOM_EMAIL_VENDOR_URL = 'http://127.0.0.1:3101/custom_email_vendor';
-  process.env.CUSTOM_EMAIL_VENDOR_USERNAME = 'USERNAME';
-  process.env.CUSTOM_EMAIL_VENDOR_PASSWORD = 'PASSWORD';
+test('delay-client-test', async (t) => {
+  const promise = delayClient.delayPromise(1000)
 
-  await emailSimpleText(logger, 'test@gmail.com', 'subject', 'body text');
-
-  const obj = await getJSON(`http://127.0.0.1:3101/lastRequest/custom_email_vendor`);
-  t.ok(obj.headers['Content-Type'] == 'application/json');
-  t.ok(obj.headers.Authorization == 'Basic VVNFUk5BTUU6UEFTU1dPUkQ=');
-  t.ok(obj.body.from == 'jambonz Support <support@jambonz.org>');
-  t.ok(obj.body.to == 'test@gmail.com');
-  t.ok(obj.body.subject == 'subject');
-  t.ok(obj.body.text == 'body text');
-
-  process.env.CUSTOM_EMAIL_VENDOR_URL = null;
-  process.env.CUSTOM_EMAIL_VENDOR_USERNAME = null;
-  process.env.CUSTOM_EMAIL_VENDOR_PASSWORD = null;
+  await promise();
+  t.pass('delayed 1 second');
+  t.end();
 });
