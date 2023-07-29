9df3fb606f8feced9542bcb89aa043a3db64e5b3
Quan HL <quan.luuhoang8@gmail.com>
2023-07-29 17:02:33 +0700

    fix

--- test/serve-integration.js
+++ test/serve-integration.js
@@ -6,7 +6,7 @@ process.on('SIGINT', async() => {
   if (stopping) return;
   stopping = true;
   console.log('shutting down');
-  // await stopDocker();
+  await stopDocker();
   process.exit(0);
 });
 
@@ -85,20 +85,18 @@ const stopDocker = () => {
   })
 };
 
-require('..');
-
-// startDocker()
-//   .then(createDb)
-//   .then(createSchema)
-//   .then(seedDb)
-//   .then(resetAdminPassword)
-//   .then(generateSipTrace)
-//   .then(() => {
-//     console.log('ready for testing!');
-//     require('..');
-//   })
-//   .catch(async(err) => {
-//     console.error({err}, 'Error running integration test');
-//     // await stopDocker();
-//     process.exit(-1);
-//   });
+startDocker()
+  .then(createDb)
+  .then(createSchema)
+  .then(seedDb)
+  .then(resetAdminPassword)
+  .then(generateSipTrace)
+  .then(() => {
+    console.log('ready for testing!');
+    require('..');
+  })
+  .catch(async(err) => {
+    console.error({err}, 'Error running integration test');
+    await stopDocker();
+    process.exit(-1);
+  });
