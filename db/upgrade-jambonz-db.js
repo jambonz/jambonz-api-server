196fb88b7903f8173d85329efda9f4736514a0bf
Quan HL <quan.luuhoang8@gmail.com>
2024-10-07 17:06:13 +0700

    update upgrade db script

--- db/upgrade-jambonz-db.js
+++ db/upgrade-jambonz-db.js
@@ -200,6 +200,8 @@ const sql = {
   ],
   9002: [
     'ALTER TABLE system_information ADD COLUMN private_network_cidr VARCHAR(8192)',
+    'ALTER TABLE system_information ADD COLUMN log_level ENUM(\'info\', \'debug\') NOT NULL DEFAULT \'info\'',
+    'ALTER TABLE accounts ADD COLUMN enable_debug_log BOOLEAN NOT NULL DEFAULT false',
   ]
 };
 
