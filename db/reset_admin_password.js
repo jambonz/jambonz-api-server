95b0f55189c268bf82a7b3de0adb4ab697e5b9e9
Quan HL <quan.luuhoang8@gmail.com>
2024-12-19 12:37:27 +0700

    update speech utils

--- db/reset_admin_password.js
+++ db/reset_admin_password.js
@@ -6,12 +6,7 @@ const sqlInsert = `INSERT into users
 (user_sid, name, email, hashed_password, force_change, provider, email_validated) 
 values (?, ?, ?, ?, ?, ?, ?)
 `;
-const sqlInsertAdminToken = `INSERT into api_keys 
-(api_key_sid, token) 
-values (?, ?)`;
-const sqlQueryAccount = 'SELECT * from accounts LEFT JOIN api_keys ON api_keys.account_sid = accounts.account_sid';
-const sqlAddAccountToken = `INSERT into api_keys (api_key_sid, token, account_sid) 
-VALUES (?, ?, ?)`;
+
 const sqlInsertPermissions = `
 INSERT into user_permissions (user_permissions_sid, user_sid, permission_sid) 
 VALUES (?,?,?)`;
@@ -36,21 +31,11 @@ const doIt = async() => {
       1
     ]
   );
-  await promisePool.execute(sqlInsertAdminToken, [uuidv4(), uuidv4()]);
 
   /* assign all permissions to the admin user */
   const [p] = await promisePool.query('SELECT * from permissions');
   for (const perm of p) {
     await promisePool.execute(sqlInsertPermissions, [uuidv4(), sid, perm.permission_sid]);
-  }
-
-  /* create admin token for single account */
-  const [r] = await promisePool.query({sql: sqlQueryAccount, nestTables: true});
-  if (1 === r.length && r[0].api_keys.api_key_sid === null) {
-    const api_key_sid = uuidv4();
-    const token = uuidv4();
-    const {account_sid} = r[0].accounts;
-    await promisePool.execute(sqlAddAccountToken, [api_key_sid, token, account_sid]);
   }
 
   process.exit(0);
