936c1a5a4376f3757ee3b05563d6498b243e4790
Rammohan Yadavalli <rammohan.yadavalli@gmail.com>
2025-01-27 16:57:15 +0530

    calling prepare-permissions in create-test-db.js

--- test/create-test-db.js
+++ test/create-test-db.js
@@ -32,3 +32,11 @@ test('add predefined carriers', (t) => {
     t.end();
   });
 });
+
+test('prepare permissions', (t) => {
+  exec(`mysql -h 127.0.0.1 -u root  --protocol=tcp --port=3360 -D jambones_test < ${__dirname}/../db/prepare-permissions-test.sql`, (err, stdout, stderr) => {
+    if (err) return t.end(err);
+    t.pass('permissions prepared');
+    t.end();
+  });
+});
