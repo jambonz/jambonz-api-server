d1f43ef95b067d186cdf0b7d1db3bf80c456d823
Sam Machin <sam@sammachin.com>
2025-06-26 16:52:45 +0100

    Update accounts.js

--- test/accounts.js
+++ test/accounts.js
@@ -339,15 +339,15 @@ test('account tests', async(t) => {
     //t.end();
 
     /* invalid Account SID */
-    //try {
-    //  result = await request.get(`/Accounts/INVALID-ACCOUNT-SID/RecentCalls?page=1&count=1`, {
-    //    auth: authAdmin,
-    //    resolveWithFullResponse: true,
-    //    json: true
-    //  });
-    //} catch (err) {
-    //  t.ok(err.statusCode === 400, 'returns 400 bad request if account sid param is not a valid uuid');
-    //}
+    try {
+      result = await request.get(`/Accounts/INVALID-ACCOUNT-SID/RecentCalls?page=1&count=1`, {
+        auth: authAdmin,
+        resolveWithFullResponse: true,
+        json: true
+      });
+    } catch (err) {
+      t.ok(err.statusCode === 400, 'returns 400 bad request if account sid param is not a valid uuid');
+    }
 
   }
   catch (err) {
