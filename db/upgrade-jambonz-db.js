848bc3b86f17f6eaf6371efbe5b06f76d4c54e5c
Sam Machin <sam@sammachin.com>
2025-06-30 11:42:29 +0100

    add migration to update existing NULL values

--- db/upgrade-jambonz-db.js
+++ db/upgrade-jambonz-db.js
@@ -228,6 +228,9 @@ const sql = {
   ],
   9004: [
     'ALTER TABLE applications ADD COLUMN env_vars TEXT',
+  ],
+  9005: [
+    'UPDATE applications SET speech_synthesis_voice = \'en-US-Standard-C\' WHERE speech_synthesis_voice IS NULL AND speech_synthesis_vendor = \'google\' AND speech_synthesis_language = \'en-US\''
   ]
 };
 
