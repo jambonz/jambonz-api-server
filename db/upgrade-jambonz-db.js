4316c75f5d6540d73474e35b10d8f0b2a107a71d
Sam Machin <sam@sammachin.com>
2025-06-30 12:03:28 +0100

    allow null

--- db/upgrade-jambonz-db.js
+++ db/upgrade-jambonz-db.js
@@ -231,7 +231,7 @@ const sql = {
   ],
   9005: [
     'UPDATE applications SET speech_synthesis_voice = \'en-US-Standard-C\' WHERE speech_synthesis_voice IS NULL AND speech_synthesis_vendor = \'google\' AND speech_synthesis_language = \'en-US\'',
-    'ALTER TABLE applications MODIFY COLUMN speech_synthesis_voice VARCHAR(255) NOT NULL DEFAULT \'en-US-Standard-C\''
+    'ALTER TABLE applications MODIFY COLUMN speech_synthesis_voice VARCHAR(255) DEFAULT \'en-US-Standard-C\''
   ]
 };
 
